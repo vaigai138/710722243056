de375bc9cd4f50d3a20400f5d20780773eff4beb
Vaigai Vendhan <133666759+vaigai138@users.noreply.github.com>
2025-03-20 13:47:38 +0530

    Update App.jsx

--- social_media/src/App.jsx
+++ social_media/src/App.jsx
@@ -1,18 +1,25 @@
-import Feed from "./components/Feed"
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import TopUsers from "./components/TopUsers";
+import Feed from "./components/Feed";
+import TrendingPosts from "./components/TrendingPosts";
 
-
-function App() {
-
+const App = () => {
   return (
-    <>
-      <div className="social-media">
-        <h1 className="text-center text-2xl font-bold">Social Media</h1>
-        <Feed/>
-                <p>The Api is not working !</p>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/" className="text-3xl font-bold text-gray-800 text-center mb-6">Top Users</Link> | <Link to="/trending" className="text-3xl font-bold text-gray-800 text-center mb-6">Trending Posts</Link> | <Link to="/feed" className="text-3xl font-bold text-gray-800 text-center mb-6">Feed</Link>
+        </nav>
 
+        <Routes>
+          <Route path="/" element={<TopUsers />} />
+          <Route path="/trending" element={<TrendingPosts />} />
+          <Route path="/feed" element={<Feed />} />
+        </Routes>
       </div>
-    </>
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;
