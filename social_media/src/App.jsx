54ad9a90cd8d262bb71975aac88e4919b7e61992
Vaigai Vendhan <133666759+vaigai138@users.noreply.github.com>
2025-03-20 12:16:04 +0530

    Update App.jsx

--- social_media/src/App.jsx
+++ social_media/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
       <div className="social-media">
         <h1 className="text-center text-2xl font-bold">Social Media</h1>
         <Feed/>
+                <p>The Api is not working !</p>
+
       </div>
     </>
   )
